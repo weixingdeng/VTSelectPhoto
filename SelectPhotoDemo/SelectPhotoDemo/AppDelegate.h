316c0f8ffca5a933143bb830b47a126ba26cfc11
dengwx <455613844@qq.com>
2016-08-15 10:02:55 +0800

    create demo

---
+++ SelectPhotoDemo/SelectPhotoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectPhotoDemo
+//
+//  Created by dengwx on 16/8/15.
+//  Copyright © 2016年 wxdeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectPhotoDemo/SelectPhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectPhotoDemo
+//
+//  Created by dengwx on 16/8/15.
+//  Copyright © 2016年 wxdeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
